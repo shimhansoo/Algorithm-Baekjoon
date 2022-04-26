f2e8938772d6e1bb0274e69c8504bfd693a3bb29
shimhansoo <103415740+shimhansoo@users.noreply.github.com>
2022-04-26 22:39:08 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a == 1)
+    {
+        printf("Leading the Way to the Future");
+    }
+    else if(a == 0)
+    {
+        printf("YONSEI");
+    }
+}
