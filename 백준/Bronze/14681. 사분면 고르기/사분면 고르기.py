03af1280b15f918fd0b0566fdb87598c5b13fc34
shimhansoo <103415740+shimhansoo@users.noreply.github.com>
2022-05-02 14:30:41 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,15 @@
+import sys
+
+input =__import__('sys').stdin.readline
+
+a = int(input())
+b = int(input())
+
+if a > 0 and b > 0 :
+    print(1)
+elif a > 0 and b < 0 :
+    print(4)
+elif a < 0 and b > 0 :
+    print(2)
+elif a < 0 and b < 0 :
+    print(3)
