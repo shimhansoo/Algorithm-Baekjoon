c20d166fe48e13ba23acc82c932031972bea0ed7
shimhansoo <103415740+shimhansoo@users.noreply.github.com>
2022-05-02 14:39:09 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.c
@@ -0,0 +1,45 @@
+int main()
+{
+    int S,K,H;
+    int min,max;
+    scanf("%d %d %d", &S, &K, &H);
+    
+    int sum = S + K + H;
+    if(sum >= 100)
+    {
+        printf("OK");
+    }
+    else if(sum < 100)
+    {
+        if(S > K)
+        {
+            max = S;
+            min = K;
+        }
+        else
+        {
+            max = K;
+            min = S;
+        }
+        if(H > max)
+        {
+            max = H;
+        }
+        if(H < min)
+        {
+            min = H;
+        }
+        if(min == S)
+        {
+            printf("Soongsil");
+        }
+        else if(min == K)
+        {
+            printf("Korea");
+        }
+        else if(min == H)
+        {
+            printf("Hanyang");
+        }
+    }
+}
