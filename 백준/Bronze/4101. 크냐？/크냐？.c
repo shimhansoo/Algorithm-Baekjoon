6e0e32123d19c7d606a882c1cafad5514cd1ba9d
shimhansoo <103415740+shimhansoo@users.noreply.github.com>
2022-05-09 15:50:09 +0900

    [Bronze III] Title: 크냐?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 1;
+    
+    while(a != 0 && b != 0)
+    {
+        scanf("%d %d", &a, &b);
+        if(a == 0 && b == 0)
+            break;
+        if( a > b)
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+    
+
+    return 0;
+}
