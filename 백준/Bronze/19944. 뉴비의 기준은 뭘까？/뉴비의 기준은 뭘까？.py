843f7d27ab0f259d10f78da811752b8a52c4cdfe
shimhansoo <103415740+shimhansoo@users.noreply.github.com>
2022-12-17 03:33:28 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.py
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.py
@@ -1,13 +1,13 @@
 import sys
 
-input =__import__('sys').stdin.readline
+input = __import__('sys').stdin.readline
 
-a,b = map(int, input().split())
+N, M = map(int,input().split())
 
-if b <= 2 :
+if ( M <= 2 ) :
     print("NEWBIE!")
-elif b > 2 :
-    if a >= b :
-        print("OLDBIE!")
-    elif a < b :
+elif ( M > 2 ) :
+    if(N < M) :
         print("TLE!")
+    elif(N >= M) :
+        print("OLDBIE!")
